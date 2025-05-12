a4c5c3f2da9f9ea0984acffe8c48057de229eb95
KCDASH01 <kcdash@gmail.com>
2025-05-12 08:04:26 +0530

    Add files via upload

---
+++ ASSIGNMENT.py
@@ -0,0 +1,12 @@
+a=input('first number: ')
+b=input('second number: ')
+a=int(a)
+b=int(b)
+ad=a+b
+sb=a-b
+x=a*b
+div=a/b
+print('addition: ',ad)
+print('subtraction: ',sb)
+print('multiplication: ',x)
+print('division: ',div)
